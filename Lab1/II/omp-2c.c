37a489bff81451879a5adc96f163a83ed69f39ba
Einscrew <manel.rodrigo.reis@gmail.com>
2018-03-15 15:00:52 +0000

    c) code

---
+++ Lab1/II/omp-2c.c
@@ -0,0 +1,65 @@
+/*
+DESCRIPTION:
+ 		Parallelizing an inner loop with dependences
+
+		for (iter=0; iter<numiter; iter++) {
+ 			for (i=0; i<size-1; i++) {
+ 				V[i] = f( V[i], V[i+1] );
+ 			}
+ 		}
+
+**************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TOTALSIZE 1000
+#define NUMITER 200
+
+/*
+ * * DUMMY FUNCTION
+ * */
+#define f(x,y)	((x+y)/2.0)
+
+
+/* MAIN: PROCESS PARAMETERS */
+int main(int argc, char *argv[]) {
+
+  /* VARIABLES */
+  int i, iter;
+
+  /* DECLARE VECTOR AND AUX DATA STRUCTURES */
+  double *V = (double *) malloc(TOTALSIZE * sizeof(double));
+  /////////////////////////////////////////////////////////
+  double *B = (double *) malloc(TOTALSIZE * sizeof(double));
+  double * temp;
+
+  /* 1. INITIALIZE VECTOR */
+  for(i = 0; i < TOTALSIZE; i++) {
+    V[i]= 0.0 + i;
+    B[i]= 0.0 + i;
+  }
+
+  /* 2. ITERATIONS LOOP */
+  for(iter = 0; iter < NUMITER; iter++) {
+    temp = V;
+    V = B;
+    B = temp;
+
+    /* 2.1. PROCESS ELEMENTS */
+    #pragma omp parallel for private(i)
+    for(i = 0; i < TOTALSIZE-1; i++) {
+      V[i] = f(B[i], B[i+1]);
+    }
+
+    /* 2.2. END ITERATIONS LOOP */
+
+  }
+
+  /* 3. OUTPUT FINAL VALUES */
+  printf("Output:\n");
+  for(i = 0; i < TOTALSIZE; i++) {
+    printf("%4d %f\n", i, V[i]);
+  }
+
+}
